cb4b1eacc3ecfae19d46af5a754f285d040da156
Pariksheet Nanda <pnanda@umich.edu>
2022-12-09 12:01:33 -0500

    clean co2 emissions data and starting joining with gapminder 2007

--- code/un_data_analysis.R
+++ code/un_data_analysis.R
@@ -122,3 +122,31 @@ gapminder_data %>%
 gapminder_data_2007 <- read_csv("data/gapminder_data.csv") %>%
   filter(year == 2007, continent == "Americas") %>%
   select(-year, -continent)
+
+# Clean up data ----
+
+#read_csv("data/co2-un-data.csv")
+co2_emissions <- read_csv("data/co2-un-data.csv", skip = 2,
+         col_names = c("region", "country", "year", "series", "value",
+                       "footnotes", "source")) %>%
+  # Shorten series variables Emissions (...) and Emissions per capita (...)
+  mutate(series = recode(series,
+                         "Emissions (thousand metric tons of carbon dioxide)" = "total_emissions",
+                         "Emissions per capita (metric tons of carbon dioxide)" = "per_capita_emissions")) %>%
+  pivot_wider(names_from = series,
+              values_from = value) %>%
+  #count(year)
+  filter(year == 2005) %>%
+  select(-year)
+
+# Joining data frames ----
+gapminder_data_2007 <- read_csv("data/gapminder_data.csv") %>%
+  filter(year == 2007, continent == "Americas") %>%
+  select(-year, -continent)
+
+View(gapminder_data_2007)
+View(co2_emissions)
+
+inner_join(gapminder_data_2007, co2_emissions, by = "country")
+
+anti_join(gapminder_data_2007, co2_emissions, by = "country")
